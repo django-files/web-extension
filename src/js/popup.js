91d1d0c5a7af81a6b04e677fc9bf472281fa936e
Shane <6071159+smashedr@users.noreply.github.com>
2023-12-26 02:09:29 -0800

    Cleanup Popup

--- src/js/popup.js
+++ src/js/popup.js
@@ -648,14 +648,20 @@ async function handleFile(name, method, data = null) {
  * @param {String} type
  */
 function showToast(message, type = 'success') {
-    console.debug(`showToast: ${type}:`, message)
-    const element = document.querySelector('.d-none .toast').cloneNode(true)
-    element.classList.add(`text-bg-${type}`)
-    element.querySelector('.toast-body').innerHTML = message
-    document.getElementById('toast-container').appendChild(element)
-    const toast = new bootstrap.Toast(element)
-    element.addEventListener('mouseover', () => toast.hide())
-    toast.show()
+    console.debug(`showToast: ${type}: ${message}`)
+    const clone = document.querySelector('.d-none .toast')
+    const container = document.getElementById('toast-container')
+    if (clone && container) {
+        const element = clone.cloneNode(true)
+        element.querySelector('.toast-body').innerHTML = message
+        element.classList.add(`text-bg-${type}`)
+        container.appendChild(element)
+        const toast = new bootstrap.Toast(element)
+        element.addEventListener('mouseover', () => toast.hide())
+        toast.show()
+    } else {
+        console.info('Missing clone or container:', clone, container)
+    }
 }
 
 /**
