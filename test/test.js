32c6c6f5ede20a853f467a6495978eb54b43286b
Shane <6071159+smashedr@users.noreply.github.com>
2024-06-25 19:07:53 -0700

    Add Test

---
+++ test/test.js
@@ -0,0 +1,80 @@
+const puppeteer = require('puppeteer')
+const path = require('path')
+const fs = require('fs')
+
+const siteUrl = 'https://intranet.cssnr.com'
+const authToken = ''
+
+let count = 1
+
+async function screenshot(page, name) {
+    if (!fs.existsSync('screenshots')) {
+        fs.mkdirSync('screenshots')
+    }
+    await page.screenshot({ path: `screenshots/${count}_${name}.png` })
+    count++
+}
+
+;(async () => {
+    const pathToExtension = path.join(process.cwd(), '../src')
+    console.log('pathToExtension:', pathToExtension)
+    const browser = await puppeteer.launch({
+        args: [
+            `--disable-extensions-except=${pathToExtension}`,
+            `--load-extension=${pathToExtension}`,
+        ],
+        // headless: false,
+        // slowMo: 100,
+        // dumpio: true,
+    })
+    console.log('browser:', browser)
+
+    // Get Service Worker
+    const workerTarget = await browser.waitForTarget(
+        (target) =>
+            target.type() === 'service_worker' &&
+            target.url().endsWith('service-worker.js')
+    )
+    const worker = await workerTarget.worker()
+    console.log('worker:', worker)
+
+    // Open Popup
+    await worker.evaluate('chrome.action.openPopup();')
+    const popupTarget = await browser.waitForTarget(
+        (target) =>
+            target.type() === 'page' && target.url().endsWith('popup.html')
+    )
+    const popupPage = await popupTarget.asPage()
+    console.log('popupPage:', popupPage)
+    popupPage.on('console', (msg) => console.log('LOG: Popup:', msg.text()))
+    await popupPage.waitForNetworkIdle()
+    await screenshot(popupPage, 'popup')
+    await popupPage.locator('[href="../html/options.html"]').click()
+
+    // Open Options
+    // await worker.evaluate('chrome.runtime.openOptionsPage();')
+    const optionsTarget = await browser.waitForTarget(
+        (target) =>
+            target.type() === 'page' && target.url().endsWith('options.html')
+    )
+    const optionsPage = await optionsTarget.asPage()
+    console.log('optionsPage:', optionsPage)
+    optionsPage.on('console', (msg) => console.log('LOG: Options:', msg.text()))
+    // await optionsPage.setViewport({ width: 1920, height: 1080 })
+    await optionsPage.waitForNetworkIdle()
+    await screenshot(optionsPage, 'options')
+
+    await optionsPage.locator('#siteUrl').fill(siteUrl)
+    // await optionsPage.type('#siteUrl', siteUrl)
+    await optionsPage.keyboard.press('Enter')
+    await optionsPage.locator('#authToken').fill(authToken)
+    // await optionsPage.type('#authToken', authToken)
+    await optionsPage.keyboard.press('Enter')
+    await optionsPage.locator('.show-hide').click()
+    await screenshot(optionsPage, 'options')
+    await optionsPage.close()
+
+    // await optionsPage.close()
+
+    await browser.close()
+})()
