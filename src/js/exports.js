d27b7b345746f6c32b2f2e889ce48c959ef2cec1
Shane <6071159+smashedr@users.noreply.github.com>
2024-10-12 13:26:07 -0700

    Update openExtPanel

--- src/js/exports.js
+++ src/js/exports.js
@@ -32,12 +32,14 @@ export function openSidePanel(event) {
  * @param {String} [url]
  * @param {Number} [width]
  * @param {Number} [height]
+ * @param {String} [type]
  * @return {Promise<chrome.windows.Window>}
  */
 export async function openExtPanel(
     url = '/html/popup.html',
     width = 0,
-    height = 0
+    height = 0,
+    type = 'panel'
 ) {
     // let size = localStorage.getItem('panel-size')?.split('x')
     // let size = await sendOffscreen('storage', { key: 'panel-size' })
@@ -48,16 +50,25 @@ export async function openExtPanel(
     width = parseInt(width || size[0] || 340)
     height = parseInt(height || size[1] || 600)
     console.debug(`openExtPanel: ${url}`, width, height)
-    const windows = await chrome.windows.getAll({ populate: true })
-    // console.debug('windows:', windows)
-    for (const window of windows) {
-        // console.debug('window:', window)
-        if (window.tabs[0]?.url?.endsWith(url)) {
-            console.debug(`%c Panel found: ${window.id}`, 'color: Lime')
-            return chrome.windows.update(window.id, { focused: true })
+    const { lastPanelID } = await chrome.storage.local.get(['lastPanelID'])
+    console.debug('lastPanelID:', lastPanelID)
+
+    try {
+        const window = await chrome.windows.get(lastPanelID)
+        if (window) {
+            console.debug(`%c Window found: ${window.id}`, 'color: Lime')
+            return await chrome.windows.update(lastPanelID, {
+                focused: true,
+            })
         }
+    } catch (e) {
+        console.log(e)
     }
-    return chrome.windows.create({ type: 'panel', url, width, height })
+
+    const window = await chrome.windows.create({ type, url, width, height })
+    console.debug(`%c Created new window: ${window.id}`, 'color: Yellow')
+    await chrome.storage.local.set({ lastPanelID: window.id })
+    return window
 }
 
 /**
